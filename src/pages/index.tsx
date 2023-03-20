bdf49aba84ab7ad786b8aefe4da3002b1d6053fa
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-03-20 22:19:25 +0100

    [tested] tailwind working fine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import Head from "next/head";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className="bg-red-400 text-3xl capitalize font-bold">
+        health shop online
+      </h1>
+    </>
+  );
 }
