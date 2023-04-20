c93c4e0e54d00906d13ca1a8433e6fc14b662a0b
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-20 17:14:34 +0100

    added toast container to root app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { CartContextProvider } from "../modules/cart/context";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartContextProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </CartContextProvider>
   );
 }
