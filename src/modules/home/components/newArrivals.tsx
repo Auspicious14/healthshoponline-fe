f9529d1a1e9718aad80d956ac9188493812131da
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-08-10 23:14:25 +0100

    [add] new arrival feature

---
+++ src/modules/home/components/newArrivals.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ApImage } from "../../../components";
+import { Button } from "antd";
+import Categories from "../../../../public/images/Frame 1000005044 (2).png";
+import Link from "next/link";
+import { helper } from "../../../helper";
+import { IProduct } from "../../product/model";
+import { useProductState } from "../../product/context";
+
+export const NewArrivals = () => {
+  const { newArrivals } = useProductState();
+
+  return (
+    <div className="grid gap-4 grid-cols-4">
+      {newArrivals?.map((p: IProduct) => (
+        <div>
+          <Link href={`/product/${p?._id}`} className="">
+            <div className="relative">
+              <div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-52">
+                <ApImage
+                  key={p?.images[0]?._id}
+                  src={p?.images[0]?.uri}
+                  alt={p?.images[0]?.name}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  unoptimized
+                />
+              </div>
+              <div className="mt-4 mx-4">
+                <h3 className=" text-gray-700 font-bold">{p.name}</h3>
+                <p className=" font-bold text-gray-900">
+                  {helper.toCurrency(parseFloat(p.price))}
+                </p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,19 @@ import { HomePage } from "../modules/home/page";
 import { apiReqHandler } from "../components";
 import { useEffect } from "react";
 import { useCategorystate } from "../modules/category/context";
+import { useProductState } from "../modules/product/context";
 
 interface IProps {
   data: any;
 }
 export default function Home({ data }: IProps) {
   const { setCategories } = useCategorystate();
+  const { setNewArrivals } = useProductState();
 
   useEffect(() => {
     if (data) {
-      setCategories(data);
+      setCategories(data.data);
+      setNewArrivals(data.newArrival);
     }
   }, [data]);
   return (
@@ -33,9 +36,17 @@ export const getServerSideProps = async ({
     endPoint: `${process.env.NEXT_PUBLIC_API_ROUTE}/category`,
     method: "GET",
   });
-  const data = await res.res?.data?.data;
 
+  const data = await res.res?.data?.data;
   console.log(data);
+
+  const response = await apiReqHandler({
+    endPoint: `${process.env.NEXT_PUBLIC_API_ROUTE}/products?newArrival=newArrival`,
+    method: "GET",
+  });
+  const newArrival = await response?.res?.data?.data;
+  console.log(newArrival, "new arrival");
+
   if (!req?.cookies.user_id) {
     return {
       redirect: {
@@ -45,6 +56,6 @@ export const getServerSideProps = async ({
     };
   }
   return {
-    props: { data },
+    props: { data: { data, newArrival } },
   };
 };
